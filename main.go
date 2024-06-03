037e273ed466e147b030255061998aa3a032fcd7
Evandro Monteiro Lima <goodvandro@gmail.com>
2024-06-03 16:32:50 +0000

    feat: Add todo

--- main.go
+++ main.go
@@ -15,8 +15,28 @@ type Todo struct {
 func main() {
 	app := fiber.New()
 
+	todos := []Todo{}
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{"msg": "hello world"})
+	})
+
+	// CREATE A TODO
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "Todo body is required"})
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.Status(201).JSON(todo)
 	})
 
 	log.Fatal(app.Listen(":4000"))
