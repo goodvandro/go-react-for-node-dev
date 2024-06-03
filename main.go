eb586fd089486f96e17044192e96b3f830c803f8
Evandro Monteiro Lima <goodvandro@gmail.com>
2024-06-03 16:03:55 +0000

    Add home route

--- main.go
+++ main.go
@@ -6,8 +6,18 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
+
 func main() {
 	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "hello world"})
+	})
 
 	log.Fatal(app.Listen(":4000"))
 }
