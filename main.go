d86b1248d1e853c432bc6a0d6a166a3afb323602
Evandro Monteiro Lima <goodvandro@gmail.com>
2024-06-19 20:56:07 +0000

    Add update todo feature

--- main.go
+++ main.go
@@ -53,8 +53,8 @@ func main() {
 
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodos)
-	// app.Patch("/api/todos", updateTodos)
-	// app.Delete("/api/todos", deleteTodos)
+	app.Patch("/api/todos/:id", updateTodos)
+	app.Delete("/api/todos/:id", deleteTodos)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -105,5 +105,20 @@ func createTodos(c *fiber.Ctx) error {
 	return c.Status(201).JSON(todo)
 }
 
-// func updateTodos(c *fiber.Ctx) error {}
+func updateTodos(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid todo ID"})
+	}
+
+	filter := bson.M{"_id": objectID}
+	update := bson.M{"$set": bson.M{"completed": true}}
+	_, err = collation.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		return err
+	}
+	return c.Status(200).JSON(fiber.Map{"success": true})
+}
+
 // func deleteTodos(c *fiber.Ctx) error {}
