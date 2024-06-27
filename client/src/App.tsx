b74fefa88bd7a9d713bf98f0ef82afbdec9c11eb
Evandro Monteiro Lima <goodvandro@gmail.com>
2024-06-27 19:21:55 +0000

    Add client side

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button, Container, Stack } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <Stack h="100vh">
+      <Navbar />
+      <Container>
+        {/* <TodoForm />
+        <TodoList /> */}
+      </Container>
+    </Stack>
+  )
+}
+
+export default App
