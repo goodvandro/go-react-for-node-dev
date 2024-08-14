58a4495308ccabee7ad780cec2d8639a92fdd523
Evandro Monteiro Lima <goodvandro@gmail.com>
2024-08-14 11:14:31 +0000

    Add todo list template

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
-import { Button, Container, Stack } from '@chakra-ui/react'
+import { Container, Stack } from '@chakra-ui/react'
+import Navbar from './components/Navbar'
+import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
 
 function App() {
   return (
     <Stack h="100vh">
       <Navbar />
       <Container>
-        {/* <TodoForm />
-        <TodoList /> */}
+        <TodoForm />
+        <TodoList />
       </Container>
     </Stack>
   )
